c3a1434a088f9c5cfc0fe24a5d89f0a0ef175f8f
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2024-07-27 00:16:00 -0300

    context on the server side

---
+++ 3-context/2-cliente-server/server/main.go
@@ -0,0 +1,34 @@
+/*
+	o servidor vai esperar 5 segundos para processar a requisição e retornar ao
+	usuário que a requisição foi processada com sucesso.
+	Se passar 5 segundos, o usuário vai receber um erro de rquest timeout.
+*/
+
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context() // pega o contexto da requisição
+	log.Println("Request iniciada")
+	defer log.Println("Request finalizada")
+
+	select {
+	case <-time.After(5 * time.Second):
+		log.Println("Processamento finalizado  com sucesso") // imprime no console
+		// imprime no browser
+		w.Write([]byte("Processamento finalizado com sucesso"))
+	case <-ctx.Done():
+		err := ctx.Err()
+		log.Println("Request cancelada pelo cliente:", err) // imprime no console
+	}
+}
